918ac2e64128c925c138f9e6acf9ea25ab06352b
David Higgins <dave.higgins@yotta.co.uk>
2018-11-25 14:31:39 +0000

    factor dependency tracking code into new class, out of resolution class

---
+++ Dependinator/DependencyManager.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Dependinator
+{
+    public class DependencyManager<T>
+    {
+        public DependencyManager()
+        {
+            UnboundedDependancyTaken = new HashSet<IDependencyState<T>>();
+            TargetsTaken = new Dictionary<T, ISet<IDependencyState<T>>>();
+            DependanciesTaken = new Dictionary<T, ISet<IDependencyState<T>>>();
+        }
+
+        private IDictionary<T, ISet<IDependencyState<T>>> TargetsTaken { get; }
+        private IDictionary<T, ISet<IDependencyState<T>>>  DependanciesTaken { get; }
+        private ISet<IDependencyState<T>> UnboundedDependancyTaken { get; }
+
+        public void Update(IDependencyState<T> state)
+        {
+            // Manage dependencies and targets taken
+            foreach (var dependancy in state.NextDependencies)
+            {
+                if (!DependanciesTaken.ContainsKey(dependancy))
+                {
+                    DependanciesTaken[dependancy] = new HashSet<IDependencyState<T>>();
+                }
+                DependanciesTaken[dependancy].Add(state);
+            }
+            if (state.NextUnboundDependency)
+            {
+                UnboundedDependancyTaken.Add(state);
+            }
+            foreach (var target in state.NextTargets)
+            {
+                if (!TargetsTaken.ContainsKey(target))
+                {
+                    TargetsTaken[target] = new HashSet<IDependencyState<T>>();
+                }
+                TargetsTaken[target].Add(state);
+            }
+        }
+
+        public List<IDependencyState<T>> UnresolvedDependencies(IDependencyState<T> state)
+        {
+            return state.NextDependencies
+                        .Where(TargetsTaken.ContainsKey)
+                        .SelectMany(t => TargetsTaken[t].Where(s => !s.Equals(state)))
+                        .Where(s => s.State == DependState.Resolving)
+                        .ToList();
+        }
+
+        public bool IsFrozen(IDependencyState<T> state)
+        {
+            var targets = TargetsTaken.Where(x => x.Value.Contains(state))
+                                      .Select(x => x.Key)
+                                      .Concat(state.NextTargets)
+                                      .ToList();
+
+            var dependingStates = targets.SelectMany(x => DependanciesTaken.Where(d => d.Key.Equals(x)).SelectMany(d => d.Value).Concat(UnboundedDependancyTaken));
+            return dependingStates.Any(x => !x.Equals(state) && !IsCompleted(x));
+        }
+
+        public bool IsCompleted(IDependencyState<T> state)
+        {
+            return state.State == DependState.Completed ||
+                   state.State == DependState.Failed;
+        }
+
+        void ResetSet(IDependencyState<T> source, ISet<IDependencyState<T>> taken)
+        {
+            var toRemove = taken.Where(x => !x.Equals(source)).ToList();
+
+            foreach (var takenState in toRemove)
+            {
+                taken.Remove(takenState);
+                takenState.Reset();
+            }
+            foreach (var takenState in toRemove)
+            {
+                ResetDependenciesOf(takenState);
+            }
+        }
+
+        // Reset anything that has taken a dependency on this state
+        public void ResetDependenciesOf(IDependencyState<T> state)
+        {
+            var targets = TargetsTaken.Where(x => x.Value.Contains(state))
+                                      .Select(x => x.Key)
+                                      .ToList();
+
+            // Reset bounded dependencies taken
+            foreach (var dependancy in targets.Where(DependanciesTaken.ContainsKey))
+            {
+                ResetSet(state, DependanciesTaken[dependancy]);
+            }
+
+            // Reset unbounded dependencies taken
+            ResetSet(state, UnboundedDependancyTaken);
+
+            // clear targets
+            foreach (var target in targets)
+            {
+                TargetsTaken[target].Remove(state);
+            }
+        }
+    }
+}
--- Dependinator/Resolver.cs
+++ Dependinator/Resolver.cs
@@ -7,35 +7,31 @@ namespace Dependinator
 {
     public class Resolver<T> 
     {
+        private DependencyManager<T> Dependencies { get; }
         private IAdvancmentStrategy<T> Strategy { get; }
-        private IDictionary<T, ISet<IDependencyState<T>>> TargetsTaken { get; }
-        private IDictionary<T, ISet<IDependencyState<T>>>  DependanciesTaken { get; }
-        private ISet<IDependencyState<T>> UnboundedDependancyTaken { get; }
         public Resolver(IAdvancmentStrategy<T> strategy)
         {
             Strategy = strategy;
-            UnboundedDependancyTaken = new HashSet<IDependencyState<T>>();
-            TargetsTaken = new Dictionary<T, ISet<IDependencyState<T>>>();
-            DependanciesTaken = new Dictionary<T, ISet<IDependencyState<T>>>();
+            Dependencies = new DependencyManager<T>();
         }
 
         public async Task Resolve()
         {
             var toAdvance = new List<IDependencyState<T>>();
 
-            while (Strategy.States.Any(x => !IsCompleted(x)))
+            while (Strategy.States.Any(x => !Dependencies.IsCompleted(x)))
             {
-                var resolving = Strategy.States.Any(x => x.State == DependState.Resolving && !IsFrozen(x));
+                var resolving = Strategy.States.Any(x => x.State == DependState.Resolving && !Dependencies.IsFrozen(x));
 
                 foreach (var state in Strategy.States)
                 {
                     // Don't advance completed states
-                    if (IsCompleted(state)) continue;
+                    if (Dependencies.IsCompleted(state)) continue;
 
                     // Frozen states what to take a target that has already been taken as a dependency (and not completed)
                     if(state.State != DependState.Resolved)
                     {
-                        if(IsFrozen(state)) continue;
+                        if(Dependencies.IsFrozen(state)) continue;
                     }
 
                     // If we are resolving...
@@ -47,8 +43,8 @@ namespace Dependinator
                         // Dont allow states to advance that depend on unresolved targets, unless
                         // this is going to cause a deadlock (then this can be the lone ranger to advance,
                         // freezing out the rest)
-                        var unresolved = UnresolvedDependencies(state);
-                        if (unresolved.Any() && unresolved.Select(UnresolvedDependencies).Any(x => x.Count == 0)) continue;
+                        var unresolved = Dependencies.UnresolvedDependencies(state);
+                        if (unresolved.Any() && unresolved.Select(Dependencies.UnresolvedDependencies).Any(x => x.Count == 0)) continue;
 
                         // We cannot allow unbound dependencies
                         if (state.NextUnboundDependency)
@@ -58,27 +54,8 @@ namespace Dependinator
                         }
                     }
 
-                    // Manage dependencies and targets taken
-                    foreach (var dependancy in state.NextDependencies)
-                    {
-                        if (!DependanciesTaken.ContainsKey(dependancy))
-                        {
-                            DependanciesTaken[dependancy] = new HashSet<IDependencyState<T>>();
-                        }
-                        DependanciesTaken[dependancy].Add(state);
-                    }
-                    if (state.NextUnboundDependency)
-                    {
-                        UnboundedDependancyTaken.Add(state);
-                    }
-                    foreach (var target in state.NextTargets)
-                    {
-                        if (!TargetsTaken.ContainsKey(target))
-                        {
-                            TargetsTaken[target] = new HashSet<IDependencyState<T>>();
-                        }
-                        TargetsTaken[target].Add(state);
-                    }
+                    // track targets and dependencies taken
+                    Dependencies.Update(state);
 
                     // This state is advancable
                     toAdvance.Add(state);
@@ -96,77 +73,12 @@ namespace Dependinator
                     // Any state that took a dependency on a failed state must be restarted
                     if(advanced.State == DependState.Failed)
                     {
-                        CheckReset(advanced);
+                        Dependencies.ResetDependenciesOf(advanced);
                     }
                 }
 
                 toAdvance.Clear();
             }
         }
-
-        void ResetSet(IDependencyState<T> source, ISet<IDependencyState<T>> taken)
-        {
-            var toRemove = taken.Where(x => !x.Equals(source)).ToList();
-
-            foreach (var takenState in toRemove)
-            {
-                taken.Remove(takenState);
-                takenState.Reset();
-            }
-            foreach (var takenState in toRemove)
-            {
-                CheckReset(takenState);
-            }
-        }
-
-        // Reset anything that has taken a dependency on this state
-        void CheckReset(IDependencyState<T> state)
-        {
-            var targets = TargetsTaken.Where(x => x.Value.Contains(state))
-                                      .Select(x => x.Key)
-                                      .ToList();
-
-            // Reset bounded dependencies taken
-            foreach (var dependancy in targets.Where(DependanciesTaken.ContainsKey))
-            {
-                ResetSet(state, DependanciesTaken[dependancy]);
-            }
-
-            // Reset unbounded dependencies taken
-            ResetSet(state, UnboundedDependancyTaken);
-
-            // clear targets
-            foreach(var target in targets)
-            {
-                TargetsTaken[target].Remove(state);
-            }
-        }
-
-        List<IDependencyState<T>> UnresolvedDependencies(IDependencyState<T> state)
-        {
-            return state.NextDependencies
-                        .Where(TargetsTaken.ContainsKey)
-                        .SelectMany(t => TargetsTaken[t].Where(s => !s.Equals(state)))
-                        .Where(s => s.State == DependState.Resolving)
-                        .ToList();
-        }
-
-        bool IsFrozen(IDependencyState<T> state)
-        {
-            var targets = TargetsTaken.Where(x => x.Value.Contains(state))
-                                      .Select(x => x.Key)
-                                      .Concat(state.NextTargets)
-                                      .ToList();
-
-            var dependingStates = targets.SelectMany(x => DependanciesTaken.Where(d => d.Key.Equals(x)).SelectMany(d => d.Value).Concat(UnboundedDependancyTaken));
-            return dependingStates.Any(x => !x.Equals(state) && !IsCompleted(x));
-        }
-
-        bool IsCompleted(IDependencyState<T> state)
-        {
-            return state.State == DependState.Completed ||
-                   state.State == DependState.Failed;
-        }
-
     }
 }
